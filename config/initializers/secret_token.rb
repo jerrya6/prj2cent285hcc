fbfe917f4bf12b25fb3c59130c7254a008b24046
JerryA <jerrya@ubuntu.(none)>
2012-03-01 14:21:18 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prj2::Application.config.secret_token = '09a756acd2ae17e4c6d381d51417aabbe1407a3fb64c416c6e3c7ca9dc6a429df9014c3c6efedf069d46e50387759de4c76c6997a2ec1a257b99d087b6a0570b'
